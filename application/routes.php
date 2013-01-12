1991c1233b91ae45644df6fe47fa86a84837f0d7
Ricardo Maia <maiams@msn.com>
2013-01-12 19:02:03 -0200

    routes

--- application/routes.php
+++ application/routes.php
@@ -37,7 +37,7 @@ Route::get('/', function()
 	return View::make('home.index');
 });
 
-Route::controller('teste');
+Route::controller(Controller::detect());
 
 /*
 |--------------------------------------------------------------------------
